2cb26ee44c05e651cb09be2dd3d6cff22746c83f
Karol Moroz <k.j.moroz@gmail.com>
2021-10-24 13:34:06 +0200

    Implement show task method

--- src/controllers/task_controller.rs
+++ src/controllers/task_controller.rs
@@ -1,7 +1,8 @@
 use crate::models::DbConn;
 use crate::models::Task;
+use crate::schema::tasks;
 use crate::schema::tasks::dsl::*;
-use diesel::RunQueryDsl;
+use diesel::prelude::*;
 use rocket::serde::json::Json;
 
 #[get("/")]
@@ -10,5 +11,16 @@ pub async fn list_tasks(conn: DbConn) -> Result<Json<Vec<Task>>, String> {
         Ok(list) => Ok(Json(list)),
         _ => Err(String::from("No can do")),
     })
+    .await
+}
+
+#[get("/<task_id>")]
+pub async fn get_task(task_id: i32, conn: DbConn) -> Option<Json<Task>> {
+    conn.run(
+        move |c| match tasks::table.find(&task_id).first::<Task>(c) {
+            Ok(task) => Some(Json(task)),
+            _ => None,
+        },
+    )
     .await
 }
--- src/router.rs
+++ src/router.rs
@@ -3,5 +3,11 @@ use rocket::Build;
 use rocket::Rocket;
 
 pub fn build() -> Rocket<Build> {
-    rocket::build().mount("/tasks", routes![controllers::task_controller::list_tasks])
+    rocket::build().mount(
+        "/tasks",
+        routes![
+            controllers::task_controller::list_tasks,
+            controllers::task_controller::get_task
+        ],
+    )
 }
